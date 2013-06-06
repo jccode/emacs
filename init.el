379b1880ea9ee559339259fc0188db045252dae8
jcchen <junchangchen@gmail.com>
2013-06-06 15:40:36 +0800

    add smooth-scrolling, ace-jump-mode

--- init.el
+++ init.el
@@ -493,6 +493,32 @@ region-end is used."
 (setq mumamo-background-colors nil)
 
 
+;; smooth-scrolling
+(require 'smooth-scrolling)
+
+
+;; ace jump mode
+(require 'ace-jump-mode)
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+;; you can select the key you prefer to
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+;; enable a more powerful jump back function from ace jump mode
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
+
+
 
 ;; ------------------------
 ;; Themes
