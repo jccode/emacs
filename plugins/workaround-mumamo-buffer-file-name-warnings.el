0ae95a81d00397f4a1ec9d7631a8426a814d6a00
jessie <junchangchen@gmail.com>
2015-09-17 15:48:27 +0800

    emacs 24.4 warning

--- plugins/workaround-mumamo-buffer-file-name-warnings.el
+++ plugins/workaround-mumamo-buffer-file-name-warnings.el
@@ -5,7 +5,7 @@
 ;; Warning (mumamo-per-buffer-local-vars):
 ;; Already 'permanent-local t: buffer-file-name
 (when (and (equal emacs-major-version 24)
-           (equal emacs-minor-version 3))
+           (>= emacs-minor-version 3))
   (eval-after-load "mumamo"
     '(setq mumamo-per-buffer-local-vars
            (delq 'buffer-file-name mumamo-per-buffer-local-vars))))
