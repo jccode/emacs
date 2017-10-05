c63c0e309e3a58e0240268ed00b8853dcb3029e0
jcchen <junchangchen@gmail.com>
2017-10-05 17:54:37 +0800

    web-mode instead of nxhtml-mode, submodule updated

--- init.el
+++ init.el
@@ -444,29 +444,29 @@ region-end is used."
 
 
 ;; web-mode
-;; (require 'web-mode)
-;; (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
-;; (defun web-mode-hook ()
-;;   "Hooks for Web mode."
-;;   (setq web-mode-markup-indent-offset 4)
-;;   (setq web-mode-css-indent-offset 4)
-;;   (setq web-mode-code-indent-offset 4)
-;;   (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "peru"))
-;; (add-hook 'web-mode-hook 'web-mode-hook)
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 4)
+  (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "peru"))
+(add-hook 'web-mode-hook 'web-mode-hook)
 
 
 ;; nxhtml
-(add-to-list 'load-path "~/emacs/plugins/nxhtml")
-(load "~/emacs/plugins/nxhtml/autostart.el")
-(setq mumamo-background-colors nil)
+;; (add-to-list 'load-path "~/emacs/plugins/nxhtml")
+;; (load "~/emacs/plugins/nxhtml/autostart.el")
+;; (setq mumamo-background-colors nil)
 
 
 
