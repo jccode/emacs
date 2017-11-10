acff350e235382b3101fab63f407b4d9e7a07753
jcchen <junchangchen@gmail.com>
2017-11-10 21:33:49 +0800

    fcitx

--- init.el
+++ init.el
@@ -937,6 +937,9 @@ region-end is used."
   '("marmalade" . "http://marmalade-repo.org/packages/") t) (package-initialize)
 
 
+;; (require 'fcitx)
+;; (fcitx-default-setup)
+
 
 ;;------------------------------
 ;; My hook
